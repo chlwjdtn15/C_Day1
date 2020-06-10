2a7159e7a16a9ce73e11536e8c0d3d71ed2ce24e
Jung Soo <chlwjdtn15@hotmail.com>
2020-06-10 02:27:55 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+  
+
+
+
+int weather = 2;
+
+  if(weather == 1) {
+    printf("우산!");
+  }
+
+
+  if (weather == 2) {
+    printf("체인!");
+  }
+
+  
+  if (weather == 3) {
+    printf("맑음!");
+  }
+  return 0;
+}
